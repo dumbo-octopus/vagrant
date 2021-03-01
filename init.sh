1f5106eccebf347e056895b7da572c79823351ed
Jakub Stawik <jakub.stawik@gmail.com>
2021-03-01 01:05:32 +0100

    Cleanup

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-function getInput {
-    read -p "Enter $1: (default: $2) " inputValue
-    export $1=${inputValue:-"$2"} 
-}
-getInput provider virtualbox
-getInput vmOS "centos/7"
-
-vagrant box add $vmOS --provider=$provider
